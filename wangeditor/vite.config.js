5eb361be64245bdf7e4c381ab3b37ee5f8a681e2
bighb <2740916018@qq.com>
2025-05-07 18:18:49 +0800

    Update vite.config.js

--- wangeditor/vite.config.js
+++ wangeditor/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  // 把本地 ip 暴露出去
+  server: {
+    host: true,
+    port: 8080,
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
